1d727da8c71a1ffa7d76263099785b6ca2eb92b8
Artem <stuzukartem12@gmail.com>
2026-02-02 00:13:30 +0200

    js

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const { name, value } = event.target;
+
+  if (name in formData) {
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+
+  form.reset();
+}
